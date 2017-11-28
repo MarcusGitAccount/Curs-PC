8dc5801b9b46ce2c9cd811cb8d5eb088a385a6c0
Marcus Pop <pop_marcus_98@yahoo.com>
2017-11-28 21:19:59 +0000

    deleted some commits

--- lab_08/src/main.c
+++ lab_08/src/main.c
@@ -6,17 +6,11 @@ uint32_t** pascals_triangle(uint32_t n) {
   for (uint32_t index = 0; index < n; index++)  
     triangle[index] = (uint32_t*)malloc(sizeof(uint32_t) * (index + 1));
     
-  triangle[0][0] = 1;
-  
-  if (n >= 2)
-    triangle[1][0] = triangle[1][1] = 1;
-    
-  for (uint32_t index = 2; index < n; index++) {
+  for (uint32_t index = 0; index < n; index++) {
     triangle[index][0] = triangle[index][index] = 1;
     
-    for (uint32_t j = 1; j < index; j++) {
+    for (uint32_t j = 1; j < index; j++)
       triangle[index][j] = triangle[index - 1][j] + triangle[index - 1][j - 1];
-    }
   }
   
   return triangle;
@@ -32,17 +26,34 @@ void free_triangle(uint32_t** triangle, uint32_t size) {
 void print_triangle(uint32_t** triangle, uint32_t size) {
   for (uint32_t index = 0; index < size; index++) {
     for (uint32_t j = 0; j <= index; j++)
-      printf("%u ", triangle[index][j]);
+      printf("%4u ", triangle[index][j]);
     printf("\n");
   }
 }
 
+int add(int a, int b) {
+  return a + b;
+}
+
+int substract(int a, int b) {
+  return a - b;
+}
+
+int (*choose(bool type))(int, int) {
+  if (type)
+    return add;
+  return substract;
+}
+
 int main(void) {
-  uint32_t   size = 10;
+  uint32_t   size = 4;
   uint32_t** triangle = pascals_triangle(size);
+  int choosen = choose(true)(2, 3);
   
   print_triangle(triangle, size);
   free_triangle(triangle, size);
-
+  puts("");
+  printf("%d\n", choosen);
+  
   return 0;
 }
