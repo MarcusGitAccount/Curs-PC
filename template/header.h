d2e1e48ae995a5bba251b958b2a477427daec6a5
Marcus Pop <pop_marcus_98@yahoo.com>
2017-11-29 08:07:56 +0000

    modified template header

--- template/header.h
+++ template/header.h
@@ -2,5 +2,17 @@
 # define HEADER_H
 
 # include <stdio.h>
+# include <stdlib.h>
+# include <stdbool.h>
+
+  typedef   signed          char  int8_t;
+  typedef   signed     short int int16_t;
+  typedef   signed           int int32_t;
+  typedef   signed long long int int64_t;
+  
+  typedef unsigned          char  uint8_t;
+  typedef unsigned     short int uint16_t;
+  typedef unsigned           int uint32_t;
+  typedef unsigned long long int uint64_t;
 
 #endif
